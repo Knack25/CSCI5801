b9ecc7f36bea6073fd9ff2c20f7f4132b1ad0884
Jarod <jarod.i.smith@gmail.com>
2021-11-29 18:39:12 -0500

    Added pantry folder/package for organization. Added pantry class and pantry adapter class. Also added pantry layout file to work on.

---
+++ app/src/main/java/com/example/wfd/pantry/Pantry.java
@@ -0,0 +1,11 @@
+package com.example.wfd.pantry;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Pantry extends AppCompatActivity {
+
+}
